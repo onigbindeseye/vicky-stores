7c7d3eff640d0db4918730514e5e7b0af0e5b4e5
onigbindeseye <seyeonigbinde87@gmail.com>
2023-01-02 19:00:36 +0100

    Header Component Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,32 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import { Button, Box, Typography, TextField } from "@mui/material"
-import ShoppingCartRounded from "@mui/icons-material/ShoppingCartRounded"
-import useStyles from "../css/useStyles"
+import React from "react";
+import { styled } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+import { Button, Box, Typography, TextField } from "@mui/material";
+import ShoppingCartRounded from "@mui/icons-material/ShoppingCartRounded";
+import useStyles from "../css/useStyles";
 
 const Header = ({ handleSearch, cart }) => {
-  const classes = useStyles()
+  const classes = useStyles();
+
+  const ColorButton = styled(Button)(() => ({
+    color: "#fff",
+    width: 125,
+    height: 45,
+    fontWeight: 600,
+    fontSize: "16px",
+    textTransform: "capitalize",
+    backgroundColor: "#735F32",
+    "&:hover": {
+      color: "#fff",
+      backgroundColor: "#C69749",
+    },
+  }));
+
   return (
     <Box className={classes.header}>
       <Box>
-        <Link to='/' className={classes.headerLinks}>
-          <Typography variant='h3' fontWeight='bold'>
+        <Link to="/" className={classes.headerLinks}>
+          <Typography variant="h3" fontWeight={700}>
             Vicky Stores
           </Typography>
         </Link>
@@ -22,21 +38,38 @@ const Header = ({ handleSearch, cart }) => {
           placeholder='Search by category'
           onChange={handleSearch}
           className={classes.textfield}
+          variant="standard"
         />
       </Box>
       <Box className={classes.navbar}>
-        <Link to='/cart' className={classes.buttons2}>
-          <Button variant='contained' startIcon={<ShoppingCartRounded />}>
+        <Box>
+          <Link underline="none" component="a" href="/" color={"#000"}>
+            Electronics
+          </Link>
+        </Box>
+        <Box>
+          <Link underline="none" component="a" href="/" color="#000">
+            Jewelry
+          </Link>
+        </Box>
+        <Box>
+          <Link underline="none" component="a" href="/" color="#000">
+            Fashion
+          </Link>
+        </Box>
+      </Box>
+      <Box>
+        <Link to="/cart">
+          <ColorButton variant="contained" startIcon={<ShoppingCartRounded />}>
             My Cart
             <span className={classes.cartBadge}>
               {cart.length > 0 && cart.length}
             </span>
-          </Button>
+          </ColorButton>
         </Link>
-        <Button variant='contained'>My Account </Button>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
