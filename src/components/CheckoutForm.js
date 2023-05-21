1dbedb38b61ffb97977d8ea532cd8d62d1732162
onigbindeseye <seyeonigbinde87@gmail.com>
2023-05-21 22:42:42 +0100

    CheckoutForm Component Redesigned

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
-import useForm from "../hooks/useForm"
-import { Typography, Box, TextField, Grid, InputLabel, Button } from "@mui/material"
-import ShoppingCartCheckoutRounded from "@mui/icons-material/ShoppingCartCheckoutRounded"
-import useStyles from "../css/useStyles"
+import useForm from "../hooks/useForm";
+import { Typography, Box, TextField, Grid, Button } from "@mui/material";
+import ShoppingCartCheckoutRounded from "@mui/icons-material/ShoppingCartCheckoutRounded";
+import Container from "../common/Container";
 
 const initialValue = {
   firstName: "",
@@ -12,18 +12,16 @@ const initialValue = {
   city: "",
   state: "",
   zip: "",
-}
+};
 
 const CheckoutForm = () => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
-  const [values, handleChanges] = useForm(initialValue)
-
-  const classes = useStyles()
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, handleChanges] = useForm(initialValue);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setShowSuccessMessage(true)
-  }
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
 
   const ColorButton = styled(Button)(() => ({
     color: "#fff",
@@ -40,119 +38,163 @@ const CheckoutForm = () => {
   }));
 
   return (
-    <Box>
-      <form onSubmit={handleSubmit} className={classes.form}>
-        <Typography variant='h3' color='#000'>
-          Checkout Form
-        </Typography>
-        <Grid item xs={12} className={classes.field}>
-          <InputLabel className={classes.label}>First Name</InputLabel>
-          <TextField
-            fullWidth
-            id='firstName'
-            placeholder='First Name'
-            name='firstName'
-            autoComplete='firstName'
-            className={classes.textField}
-            variant='outlined'
-            value={values.firstName}
-            onChange={handleChanges}
-          />
-        </Grid>
-        <Grid item xs={12} className={classes.field}>
-          <InputLabel className={classes.label}>Last Name</InputLabel>
-          <TextField
-            fullWidth
-            id='lastName'
-            placeholder='Last Name'
-            name='lastName'
-            autoComplete='lastName'
-            className={classes.textField}
-            variant='outlined'
-            value={values.lastName}
-            onChange={handleChanges}
-          />
-        </Grid>
-        <Grid item xs={12} className={classes.field}>
-          <InputLabel className={classes.label}>Address</InputLabel>
-          <TextField
-            fullWidth
-            id='address'
-            placeholder='Address'
-            name='address'
-            autoComplete='address'
-            className={classes.textField}
-            variant='outlined'
-            value={values.address}
-            onChange={handleChanges}
-          />
-        </Grid>
-        <Grid item xs={12} className={classes.field}>
-          <InputLabel className={classes.label}>City</InputLabel>
-          <TextField
-            fullWidth
-            id='city'
-            placeholder='City'
-            name='city'
-            autoComplete='city'
-            className={classes.textField}
-            variant='outlined'
-            value={values.city}
-            onChange={handleChanges}
-          />
-        </Grid>
-        <Grid item xs={12} className={classes.field}>
-          <InputLabel className={classes.label}>State</InputLabel>
-          <TextField
-            fullWidth
-            id='state'
-            placeholder='State'
-            name='state'
-            autoComplete='state'
-            className={classes.textField}
-            variant='outlined'
-            value={values.state}
-            onChange={handleChanges}
-          />
-        </Grid>
-        <Grid item xs={12} className={classes.field}>
-          <InputLabel className={classes.label}>Zip</InputLabel>
-          <TextField
-            fullWidth
-            id='zip'
-            placeholder='Zip'
-            name='zip'
-            autoComplete='zip'
-            className={classes.textField}
-            variant='outlined'
-            value={values.zip}
-            onChange={handleChanges}
-          />
-        </Grid>
-        <ColorButton variant='contained' startIcon={<ShoppingCartCheckoutRounded />}>
-          Checkout
-        </ColorButton>
-      </form>
+    <Container>
+      <Box
+        marginTop={{ xs: "40%", sm: "10%" }}
+        paddingBottom={{ xs: "10%", sm: "5%" }}
+      >
+        <form onSubmit={handleSubmit}>
+          <Typography variant="h4" fontWeight={700}>
+            Checkout Form
+          </Typography>
+          <Grid container spacing={1} paddingTop={{ xs: "5%", sm: "3%" }}>
+            <Grid item xs={12} sm={4} sx={{ marginBottom: 2 }}>
+              <Typography
+                variant={"subtitle2"}
+                sx={{ marginBottom: 2 }}
+                align={"left"}
+              >
+                First Name
+              </Typography>
+              <TextField
+                fullWidth
+                id="firstName"
+                placeholder="First Name"
+                name="firstName"
+                autoComplete="firstName"
+                variant="outlined"
+                value={values.firstName}
+                onChange={handleChanges}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Typography
+                variant={"subtitle2"}
+                sx={{ marginBottom: 2 }}
+                align={"left"}
+              >
+                Last Name
+              </Typography>
+              <TextField
+                fullWidth
+                id="lastName"
+                placeholder="Last Name"
+                name="lastName"
+                autoComplete="lastName"
+                variant="outlined"
+                value={values.lastName}
+                onChange={handleChanges}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Typography
+                variant={"subtitle2"}
+                sx={{ marginBottom: 2 }}
+                align={"left"}
+              >
+                Address
+              </Typography>
+              <TextField
+                fullWidth
+                id="address"
+                placeholder="Address"
+                name="address"
+                autoComplete="address"
+                variant="outlined"
+                value={values.address}
+                onChange={handleChanges}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Typography
+                variant={"subtitle2"}
+                sx={{ marginBottom: 2 }}
+                align={"left"}
+              >
+                City
+              </Typography>
+              <TextField
+                fullWidth
+                id="city"
+                placeholder="City"
+                name="city"
+                autoComplete="city"
+                variant="outlined"
+                value={values.city}
+                onChange={handleChanges}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Typography
+                variant={"subtitle2"}
+                sx={{ marginBottom: 2 }}
+                align={"left"}
+              >
+                State
+              </Typography>
+              <TextField
+                fullWidth
+                id="state"
+                placeholder="State"
+                name="state"
+                autoComplete="state"
+                variant="outlined"
+                value={values.state}
+                onChange={handleChanges}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Typography
+                variant={"subtitle2"}
+                sx={{ marginBottom: 2 }}
+                align={"left"}
+              >
+                Zip
+              </Typography>
+              <TextField
+                fullWidth
+                id="zip"
+                placeholder="Zip"
+                name="zip"
+                autoComplete="zip"
+                variant="outlined"
+                value={values.zip}
+                onChange={handleChanges}
+              />
+            </Grid>
+            <Grid marginTop={{ xs: "4%", sm: "2%" }}>
+              <ColorButton
+                variant="contained"
+                startIcon={<ShoppingCartCheckoutRounded />}
+              >
+                Checkout
+              </ColorButton>
+            </Grid>
+          </Grid>
+        </form>
 
-      {showSuccessMessage && (
-        <Box className={classes.successMessage} data-testid='successMessage'>
-          <Typography variant='h6'>
-            You have ordered some items! Woo-hoo! <span role='img'>🎉</span>
-          </Typography>
-          <Typography variant='h6'>Your orders will be shipped to:</Typography>
-          <br />
-          <br />
-          <Typography variant='subtitle2'>
-            {values.firstName} {values.lastName}
-          </Typography>
-          <Typography variant='subtitle2'>{values.address}</Typography>
-          <Typography variant='subtitle2'>
-            {values.city}, {values.state} {values.zip}
-          </Typography>
-        </Box>
-      )}
-    </Box>
-  )
-}
+        {showSuccessMessage && (
+          <Box data-testid="successMessage">
+            <Typography variant="h6">
+              You have ordered some items! Woo-hoo! <span role="img">🎉</span>
+            </Typography>
+            <Typography variant="h6">
+              Your orders will be shipped to:
+            </Typography>
+            <br />
+            <br />
+            <Typography variant="subtitle2">
+              {values.firstName} {values.lastName}
+            </Typography>
+            <Typography variant="subtitle2">{values.address}</Typography>
+            <Typography variant="subtitle2">
+              {values.city}, {values.state} {values.zip}
+            </Typography>
+          </Box>
+        )}
+      </Box>
+    </Container>
+  );
+};
 
-export default CheckoutForm
+export default CheckoutForm;
